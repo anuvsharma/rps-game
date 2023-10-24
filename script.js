412065b5afcedf2801038644b146449c4c739a0f
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-10-23 17:06:18 -0700

    added playRound function
    this function will play a round of rock paper scissors with the computers and players choice as parameters

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function playRound(playerChoice, computerChoice){
+    if (playerChoice == computerChoice){
+        return "It's a draw"
+    }
+    else if (playerChoice == "rock") {
+        if (computerChoice == "scissors"){
+            return "Player Wins"
+        }
+        else{
+            return "Computer Wins"
+        }
+    }
+    else if (playerChoice == "paper") {
+        if (computerChoice == "rock"){
+            return "Player Wins"
+        }
+        else{
+            return "Computer Wins"
+        }
+    }
+    else if (playerChoice == "scissors") {
+        if (computerChoice == "paper"){
+            return "Player Wins"
+        }
+        else{
+            return "Computer Wins"
+        }
+    }
+}
+
 function getComputerChoice(){
     let numberChoice = Math.floor(Math.random() * 3)
     if (numberChoice == 0){
@@ -10,3 +40,4 @@ function getComputerChoice(){
         return "scissors"
     } 
 }
+
